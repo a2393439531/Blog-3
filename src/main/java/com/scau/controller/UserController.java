50ea1782bd6523b6f9740375c579110de5e86e37
beyondboy <xuguoli.scau@gmail.com>
2016-05-24 20:38:50 +0800

    基本搭建成Blog项目

---
+++ src/main/java/com/scau/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.scau.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: beyondboy
+ * @Gmail: xuguoli.scau@gamil.com
+ * @Data: 2016/5/22
+ * @Time: 21:08
+ */
+@Controller
+public class UserController {
+}
